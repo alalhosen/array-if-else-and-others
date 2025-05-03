2198c0266e7c08dc9c8d43a74dec491fa2a2eb51
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-03 21:55:34 +0600

    text change

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,19 @@
 import { signInWithPopup } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import { GoogleAuthProvider } from "firebase/auth";
 import React from "react";
 import auth from "../../firebase/firebase.init";
 
 const provider = new GoogleAuthProvider();
+
 const handleGoogleSignIn = () => {
-  signInWithPopup(auth)
+  console.log(auth,provider)
+  signInWithPopup(auth, provider)
+    .then((resut) => {
+      console.log(resut);
+    })
+    .catch((error) => {
+      console.log("ERROR", error);
+    });
 };
 
 const Login = () => {
