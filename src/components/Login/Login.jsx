fe7b2e7a36e169b7cb63bf84532bfe7174034089
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-04 21:47:47 +0600

    function delete & work again

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,11 @@
-import { signInWithPopup } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
-import React from "react";
-import auth from "../../firebase/firebase.init";
-
-const provider = new GoogleAuthProvider();
-
-const handleGoogleSignIn = () => {
-  console.log(auth,provider)
-  signInWithPopup(auth, provider)
-    .then((resut) => {
-      console.log(resut);
-    })
-    .catch((error) => {
-      console.log("ERROR", error);
-    });
-};
 
 const Login = () => {
+
+
+const handleGoogleSignIn = () => {
+
+};
+
   return (
     <div>
       <button onClick={handleGoogleSignIn}>Login with google</button>
