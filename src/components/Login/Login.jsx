249e6ad0aa1bc088e1f4e327c1a8803ea1c4d51c
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-06 17:49:51 +0600

    function change

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,9 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../../firebase/firebase.init";
+
 
 const Login = () => {
-  const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
-    .then(result=>{
-      console.log(result)
-    })
-    .catch(error=>{
-      console.log('ERROR', error)
-    })
+  
   };
 
   return (
