c20b9cd3162d4a12cbc408d85899bbf8479686e0
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-06 23:41:45 +0600

    signInWithPopUp

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,13 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../firebase/firebase.init";
 
 
 const Login = () => {
 
+const provider = new GoogleAuthProvider();
+
   const handleGoogleSignIn = () => {
-  
+  signInWithPopup(auth, provider)
   };
 
   return (
