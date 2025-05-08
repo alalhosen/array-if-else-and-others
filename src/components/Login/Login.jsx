bd58a9507adbd0b28b5bd4d43af16edf5888b55c
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-08 23:27:25 +0600

    setUser

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,20 +2,21 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../firebase/firebase.init";
 import { useState } from "react";
 
-
 const Login = () => {
-  const [user, setUser] =useState(null);
+  const [user, setUser] = useState(null);
 
-const provider = new GoogleAuthProvider();
+  const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
-  signInWithPopup(auth, provider)
-  .then(result =>{
-    console.log(result.user)
-    .catch(error=>{
-      console.log('ERROR', error);
-    })
-  })
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+        setUser(null);
+      });
   };
 
   return (
