42018d01f662a5406f81e430b2080894acdbe0e4
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-10 23:54:35 +0600

    style add

--- src/__steps__.js
+++ src/__steps__.js
@@ -1,5 +1,12 @@
 /**
- *  1. install firebase: npm install firebase
+ *  1. first install firebase: npm install firebase  
+ * 2. console.firebase.google.com create a project
+ * 3. Register project 
+ * 4. git firebase config and  do not save config on the client side
+ * 5. create auth where it will be added
+ * 6. export auth from the init file
+ * 7. in the console authentication > started > enable Google Provider
+ * 8. 
  * 
  * 
  * **/
